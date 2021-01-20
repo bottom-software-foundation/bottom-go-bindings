40978c27e7e4222a5d1a7abdc53fb4c9e2285bc5
Nadir <chowdhurynadir0@outlook.com>
2021-01-20 19:05:35 +0000

    reflect testing from reference

--- bottom_test.go
+++ bottom_test.go
@@ -1,23 +1,39 @@
 package bottom
 
 import (
-	"fmt"
+	"testing"
 )
 
-func main() {
-	encodeTest := encode("test")
-	fmt.Println(encodeTest)
+type testCase struct {
+	value    string
+	expected string
+	bytes    bool
+}
 
-	decodeTest, err := decode("💖💖✨🥺,👉👈💖💖,👉👈💖💖✨🥺👉👈💖💖✨🥺,👉👈")
-	if err != nil {
-		return
-	}
-	fmt.Println(decodeTest)
+var testCases [4]testCase = [4]testCase{
+	{"Test", "💖✨✨✨,,,,👉👈💖💖,👉👈💖💖✨🥺👉👈💖💖✨🥺,👉👈", false},
+	{"h", "💖💖,,,,👉👈", true},
+	{"🥺", "🫂✨✨✨✨👉👈💖💖💖🥺,,,,👉👈💖💖💖✨🥺👉👈💖💖💖✨✨✨🥺,👉👈", false},
+	{"がんばれ", "🫂✨✨🥺,,👉👈💖💖✨✨🥺,,,,👉👈💖💖✨✨✨✨👉👈🫂✨✨🥺,,👉👈💖💖✨✨✨👉👈💖💖✨✨✨✨🥺,,👉👈🫂✨✨🥺,,👉👈💖💖✨✨🥺,,,,👉👈💖💖💖✨✨🥺,👉👈🫂✨✨🥺,,👉👈💖💖✨✨✨👉👈💖💖✨✨✨✨👉👈", false},
+}
 
-	decodeTest2, err := decode("erroneous data")
-	if err != nil {
-		fmt.Println("failed to decode.")
-		return
+func main(t *testing.T) {
+	for _, test := range testCases {
+		encoded := ""
+
+		if test.bytes {
+			encoded = encodeBytes(test.value)
+		} else {
+			encoded = encode(test.value)
+		}
+
+		if encoded != test.expected {
+			t.Fatalf("%s failed to encode into %s. Found %s instead.", test.value, test.expected, encoded)
+		} else {
+			decoded, err := decode(test.expected)
+			if err != nil {
+				t.Fatalf("%s failed to decode into %s. Found %s instead.", test.expected, test.value, decoded)
+			}
+		}
 	}
-	fmt.Println(decodeTest2)
 }
